0caf19d40739d29dffb4fc64c856fac7a62cf10f
turalbabayev <turalbabayev@outlook.com.tr>
2024-09-27 16:49:50 +0300

    Initial Commit

---
+++ NavigationControllerOdev4/PageYViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PageYViewController.swift
+//  NavigationControllerOdev4
+//
+//  Created by Tural Babayev on 27.09.2024.
+//
+
+import UIKit
+
+class PageYViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func goToMain(_ sender: UIButton) {
+        
+    }
+    
+
+}
---
+++ NavigationControllerOdev4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationControllerOdev4
+//
+//  Created by Tural Babayev on 27.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationController?.isNavigationBarHidden = true
+    }
+
+
+}
+
